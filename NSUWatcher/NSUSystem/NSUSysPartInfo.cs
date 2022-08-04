f954b6065e276bbd4204fbfa5688697748a512da
GarmusD <garmusd@gmail.com>
2022-08-04 23:53:41 +0200

    Moved to its own file

---
+++ NSUWatcher/NSUSystem/NSUSysPartInfo.cs
@@ -0,0 +1,16 @@
+﻿using NSUWatcher.NSUSystem.NSUSystemParts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSUWatcher.NSUSystem
+{
+    public class NSUSysPartInfo
+    {
+        public PartsTypes PartType;
+        public string[] AcceptableCmds;
+        public NSUSysPartsBase Part;
+    };
+}
