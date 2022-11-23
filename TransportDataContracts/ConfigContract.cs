3a1996c7db9ba39aa127f0488119ddb65d617e14
GarmusD <garmusd@gmail.com>
2022-11-23 22:34:15 +0100

    Serial transport data contracts. Initial commit.

---
+++ TransportDataContracts/ConfigContract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransportDataContracts
+{
+    public class ConfigContract
+    {
+        public string ComPort { get; set; } = string.Empty;
+        public int BaudRate { get; set; } = 0;
+        public RebootMcuConfig RebootMcu { get; set; } = new RebootMcuConfig();
+    }
+
+    public class RebootMcuConfig
+    {
+        public int BaudRate { get; set; } = 0;
+        public bool DtrPulseOnly { get; set; } = false;
+        public int Delay { get; set; }
+        public int ReconnectDelay { get; set; }
+    }
+}
