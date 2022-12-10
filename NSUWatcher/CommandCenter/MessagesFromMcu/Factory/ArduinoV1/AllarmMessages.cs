fd3e926743677eddb51bd6b14fbf9d579a9854c2
GarmusD <garmusd@gmail.com>
2022-12-10 21:26:52 +0100

    Supported more commands from mcu

---
+++ NSUWatcher/CommandCenter/MessagesFromMcu/Factory/ArduinoV1/AllarmMessages.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using NSUWatcher.CommandCenter.MessagesFromMcu.Factory.ArduinoV1.Data;
+using NSUWatcher.Interfaces.MCUCommands;
+using static NSU.Shared.JKeys;
+
+namespace NSUWatcher.CommandCenter.MessagesFromMcu.Factory.ArduinoV1
+{
+    public class AllarmMessages : IFromArduinoV1Base
+    {
+        public IMessageFromMcu TryFindMessage(JObject command)
+        {
+            string source = (string)command[Generic.Source];
+            if (source != Alarm.TargetName) return null;
+
+            string action = (string)command[Generic.Action];
+            return action switch
+            {
+                Action.Snapshot => command.ToObject<AlarmSnapshot>(),
+                _ => null,
+            };
+        }
+    }
+}
---
+++ NSUWatcher/CommandCenter/MessagesFromMcu/Factory/ArduinoV1/TempTriggerMessages.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using NSU.Shared;
+using NSUWatcher.CommandCenter.MessagesFromMcu.Factory.ArduinoV1.Data;
+using NSUWatcher.Interfaces.MCUCommands;
+
+namespace NSUWatcher.CommandCenter.MessagesFromMcu.Factory.ArduinoV1
+{
+    public class TempTriggerMessages : IFromArduinoV1Base
+    {
+        public IMessageFromMcu TryFindMessage(JObject command)
+        {
+            string source = (string)command[JKeys.Generic.Source];
+            if (source != JKeys.TempTrigger.TargetName) return null;
+
+            string action = (string)command[JKeys.Generic.Action];
+            return action switch
+            {
+                JKeys.Action.Snapshot => command.ToObject<TempTriggerSnapshot>(),
+                JKeys.Action.Info => command.ToObject<TempTriggerInfo>(),
+                _ => null,
+            };
+        }
+    }
+}
