eebc16df8d9141653cfc51ce59e8b818d8d3e6b2
GarmusD <garmusd@gmail.com>
2022-12-11 11:24:07 +0100

    Use TestTransport if debugging on Windows

--- NSUWatcher/Program.cs
+++ NSUWatcher/Program.cs
@@ -7,13 +7,15 @@ using Serilog;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
-using System.Threading.Tasks;
 using NSUWatcher.NSUSystem;
-using Microsoft.AspNetCore.Hosting;
-using NSUWatcher.Logger;
 using NSUWatcher.Interfaces;
 using NSUWatcher.CommandCenter;
 using NSUWatcher.NSUUserManagement;
+using NSUWatcher.Transport.Serial;
+using NSUWatcher.Worker;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using NSUWatcher.Transport.TestTransport;
 using NSUWatcher.NSUWatcherNet;
 
 namespace NSUWatcher
@@ -21,19 +23,35 @@ namespace NSUWatcher
     class Program
     {
         public const string APP_VERSION = "0.5.1";
+        private const string AppSettingsFile = "/etc/nsuwatcher/appsettings.json";
+        private const string AppSettingsDevFile = "/etc/nsuwatcher/appsettings.Development.json";
 
         static void Main(string[] args)
         {
+            if(Environment.OSVersion.Platform != PlatformID.Unix)
+                Environment.SetEnvironmentVariable("DOTNET_ENVIRONMENT", "Development");
+
             SetupDefaultLang();
             AppDomain.CurrentDomain.ProcessExit += ProcessExitRequested;
 
             // Not in all cases the NSUWatcher needed to run in Hosted environment.
             // So, loading config separatelly for System.CommandLine.
-            // config will be injected to a host.
+            // Config will be injected to a host when needed.
+            if (!File.Exists(AppSettingsFile))
+            {
+                Console.WriteLine($"Required settings file '{AppSettingsFile}' could not be found.");
+                return;
+            }
+            using (var fr = File.OpenRead(AppSettingsFile))
+            {
+                Console.WriteLine("Settings file is readable.");
+            }
+
+
             var config = new ConfigurationBuilder()
                 .AddEnvironmentVariables()
-                .AddJsonFile("/etc/nsuwatcher/appsettings.json", false, false)
-                .AddJsonFile("/etc/nsuwatcher/appsettings.Development.json", true, false)
+                .AddJsonFile(AppSettingsFile, false, false)
+                .AddJsonFile(AppSettingsDevFile, true, false)
                 .Build();
 
             var rootCmd = SetupCommandLine(config);
@@ -43,7 +61,7 @@ namespace NSUWatcher
             Log.CloseAndFlush();
         }
 
-        private static void ProcessExitRequested(object? sender, EventArgs e)
+        private static void ProcessExitRequested(object sender, EventArgs e)
         {
             //host shutdown
         }
@@ -76,11 +94,10 @@ namespace NSUWatcher
                 userCmd,
             };
             rootCommand.SetHandler(
-                (loggerValue) =>
+                () =>
                 {
-                    ExecCommandRunAsCLI(config, loggerValue);
-                },
-                LoggerBinder.ConfigureWithConsole(config)
+                    ExecCommandRunAsCLI(config, NSUWatcher.Logger.LoggerFactory.CreateWithConsole(config));
+                }
             );
             return rootCommand;
         }
@@ -91,11 +108,11 @@ namespace NSUWatcher
             var lockFileOption = new Option<FileInfo>(new string[] { "--lockfile", "-l" }) { IsRequired = true };
             serviceCmd.AddOption(lockFileOption);
             serviceCmd.SetHandler(
-                (lockFileValue, loggerValue) =>
+                (lockFileValue) =>
                 {
-                    ExecCommandRunAsService(lockFileValue, config, loggerValue);
+                    ExecCommandRunAsService(lockFileValue, config, NSUWatcher.Logger.LoggerFactory.Create(config));
                 },
-                lockFileOption, LoggerBinder.Configure(config)
+                lockFileOption
             );
             return serviceCmd;
         }
@@ -123,11 +140,11 @@ namespace NSUWatcher
                 optionRemoveUser
             };
             userCmdRemove.SetHandler(
-                (userValue, loggerValue) =>
+                (userValue) =>
                 {
-                    ExecCommandRemoveUser(userValue, loggerValue);
+                    ExecCommandRemoveUser(userValue, config, Logger.LoggerFactory.CreateWithConsole(config));
                 },
-                optionRemoveUser, LoggerBinder.ConfigureWithConsole(config));
+                optionRemoveUser);
             return userCmdRemove;
         }
 
@@ -144,40 +161,46 @@ namespace NSUWatcher
                 argumentIsAdmin
             };
             userCmdAdd.SetHandler(
-                (userValue, passwordValue, isAdminValue, loggerValue) =>
+                (userValue, passwordValue, isAdminValue) =>
                 {
-                    ExecCommandAddUser(userValue, passwordValue, isAdminValue, loggerValue);
+                    ExecCommandAddUser(userValue, passwordValue, isAdminValue, config, Logger.LoggerFactory.CreateWithConsole(config));
                 },
-                optionAddUserName, optionAddPassword, argumentIsAdmin, LoggerBinder.ConfigureWithConsole(config)
+                optionAddUserName, optionAddPassword, argumentIsAdmin
             );
             return userCmdAdd;
         }
 
-        private static void ExecCommandRunAsService(FileInfo lockFileInfo, IConfigurationRoot config, ILogger logger)
+        private static void ExecCommandRunAsService(FileInfo lockFileInfo, IConfigurationRoot config, Serilog.ILogger logger)
         {
             // Workaround for Linux 14.04 Upstart system. Starting 15 Ubuntu uses systemd
             // and Host can be configured to use Systemd (using Systemd extension).
             // So, a [lock] file with PID inside is needed.
 
-            int pid = System.Diagnostics.Process.GetCurrentProcess().Id;
-            File.WriteAllText(lockFileInfo.FullName, pid.ToString());
-            // Blocking call
-            RunAsService(config, logger);
-            // Delete lock file
-            if (File.Exists(lockFileInfo.FullName)) File.Delete(lockFileInfo.FullName);
+            try
+            {
+                int pid = System.Diagnostics.Process.GetCurrentProcess().Id;
+                File.WriteAllText(lockFileInfo.FullName, pid.ToString());
+                // Blocking call
+                RunAsService(config, logger);
+            }
+            finally
+            {
+                // Delete lock file
+                if (File.Exists(lockFileInfo.FullName)) File.Delete(lockFileInfo.FullName);
+            }
         }
 
-        private static void ExecCommandRunAsCLI(IConfigurationRoot config, ILogger logger)
+        private static void ExecCommandRunAsCLI(IConfigurationRoot config, Serilog.ILogger logger)
         {
             RunAsCLI(config, logger);
         }
 
-        private static void ExecCommandAddUser(string user, string password, bool isAdmin, ILogger logger)
+        private static void ExecCommandAddUser(string user, string password, bool isAdmin, IConfigurationRoot config, Serilog.ILogger logger)
         {
             CreateUser(user, password, isAdmin, logger);
         }
 
-        private static void ExecCommandRemoveUser(string user, ILogger logger)
+        private static void ExecCommandRemoveUser(string user, IConfigurationRoot config, Serilog.ILogger logger)
         {
             DeleteUser(user, logger);
         }
@@ -187,13 +210,13 @@ namespace NSUWatcher
          * Execute cli commands
          * 
          */
-        private static void CreateUser(string userName, string password, bool isAdmin, ILogger logger)
+        private static void CreateUser(string userName, string password, bool isAdmin, Serilog.ILogger logger)
         {
             logger.Information($"Creating user '{userName}'. IsAdmin: {isAdmin}");
             throw new NotImplementedException();
         }
 
-        private static void DeleteUser(string userName, ILogger logger)
+        private static void DeleteUser(string userName, Serilog.ILogger logger)
         {
             logger.Information($"Deleting user '{userName}':");
             throw new NotImplementedException();
@@ -204,73 +227,127 @@ namespace NSUWatcher
          * Execute app in hosted environment - cli or service
          * 
          */
-        private static IHostBuilder CreateHostBuilder(IConfigurationRoot configDI, ILogger logger)
+        private static IHostBuilder CreateHostBuilder(IConfigurationRoot configDI, Serilog.ILogger logger)
         {
             return Host.CreateDefaultBuilder()
+                .ConfigureLogging((builder) =>
+                {
+                    builder.Services.RemoveAll<ILoggerProvider>();
+                    builder.AddSerilog(logger);
+                    builder.SetMinimumLevel(LogLevel.Trace);
+                })
+
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
-                    hostingContext.Configuration = configDI;
+                    logger.Verbose("Injecting configuration...");
+                    config.AddConfiguration(configDI);
                 })
                 .ConfigureServices((services) =>
                 {
-                    services.AddSingleton<INsuUsers, NSUUsers>();
-                    services.AddHostedService<CmdCenter>();
-                    services.AddHostedService<NsuSystem>();
-                    services.AddHostedService<WatcherNet>();
-                    //services.AddSingleton<MyDbProviders>
+                    logger.Verbose("Adding singletons and serivces...");
+                    services
+                        .AddSingleton<INsuUsers, NSUUsers>();
+                    if (Environment.OSVersion.Platform == PlatformID.Unix)
+                    {
+                        services
+                        .AddSingleton<SerialTransport>()
+                        .AddSingleton<IMcuMessageTransport>(s => s.GetRequiredService<SerialTransport>());
+                    }
+                    else
+                    {
+                        services
+                        .AddSingleton<TestTransport>()
+                        .AddSingleton<IMcuMessageTransport>(s => s.GetRequiredService<TestTransport>());
+                    }
+                    services
+                        .AddSingleton<CmdCenter>()
+                        .AddSingleton<ICmdCenter>(s => s.GetRequiredService<CmdCenter>())
+                        .AddSingleton<NsuSystem>()
+                        .AddSingleton<INsuSystem>(s => s.GetRequiredService<NsuSystem>());
+
+                    services
+                        .AddHostedService(s => s.GetRequiredService<CmdCenter>())
+                        .AddHostedService(s => s.GetRequiredService<NsuSystem>());
+                    if (Environment.OSVersion.Platform == PlatformID.Unix)
+                    {
+                        services
+                        .AddHostedService(s => s.GetRequiredService<SerialTransport>());
+                    }
+                    else
+                    {
+                        services
+                        .AddHostedService(s => s.GetRequiredService<TestTransport>());
+                    }
+                    services
+                        .AddHostedService<NSUWorker>()
+                        .AddHostedService<WatcherNet>();
+                    logger.Verbose("Adding singletons and serivces... Done.");
                 })
-                .UseSerilog(logger)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                //.ConfigureWebHostDefaults(webBuilder =>
+                //{
+                //    webBuilder.UseStartup<Startup>();
+                //})
+                ;
         }
 
-        private static void RunAsService(IConfigurationRoot config, ILogger logger)
+        private static void RunAsService(IConfigurationRoot config, Serilog.ILogger logger)
         {
-            IHost host = CreateHostBuilder(config, logger).Build();
+            using IHost host = CreateHostBuilder(config, logger).Build();
             host.Run();
         }
 
-        private static void RunAsCLI(IConfigurationRoot config, ILogger logger)
+        private static void RunAsCLI(IConfigurationRoot config, Serilog.ILogger logger)
         {
+            bool isDevelopmentEnv = true;
             try
             {
+                global::NSUWatcher.Logger.LoggerFactory.ConsoleLevelSwitch.MinimumLevel = Serilog.Events.LogEventLevel.Debug;
+                logger.Information($"NSUWatcher is running on {Environment.OSVersion.VersionString}.");
                 logger.Information("NSUWatcher is running in console mode.");
                 logger.Information("Enter 'q' or Ctrl+C to quit.");
 
                 var builder = CreateHostBuilder(config, logger).UseConsoleLifetime();
-                var host = builder.Build();
-                logger.Information("IHost builded. Calling StartAsync().");
+                using var host = builder.Build();
                 host.Start();
-
+                
                 bool done = false;
 
+                var env = host.Services.GetRequiredService<IHostEnvironment>();
+                isDevelopmentEnv = env.IsDevelopment();
+
+                var lifetime = host.Services.GetRequiredService<IHostApplicationLifetime>();
                 var cmdCenter = (ICmdCenter)host.Services.GetRequiredService(typeof(ICmdCenter));
-                if(cmdCenter is null)
+                if (cmdCenter is null)
                 {
                     logger.Error("Cannot get an required service \"ICmdCenter\" for manual command execution.");
                     done = true;
                 }
 
+                logger.Debug("Entering Console.ReadLine() loop.");
                 while (!done)
                 {
                     string str = Console.ReadLine();
-                    if (str.Equals("q", StringComparison.OrdinalIgnoreCase))
+                    if (str == null || str.Equals("q", StringComparison.OrdinalIgnoreCase))
                     {
                         done = true;
+                        lifetime.StopApplication();
                     }
                     else
                     {
-                        cmdCenter!.ExecManualCommand(str);
+                        cmdCenter.ExecManualCommand(str);
                     }
                 }
                 logger.Information("Quit requested. Terminating...");
-                host.Dispose();
+                lifetime.StopApplication();
             }
             catch (Exception ex)
             {
-                logger.Fatal("NSUWatcher CRASH!!!! {ex}", ex);
+                logger.Fatal("NSUWatcher CRASH!!!! {ExMessage}", ex);
+            }
+            if (isDevelopmentEnv)
+            {
+                Console.WriteLine("Press enter to exit...");
+                Console.ReadLine();
             }
         }
     }
