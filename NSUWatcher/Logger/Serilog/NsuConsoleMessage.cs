94005ab678c219ace3e97096996ebe8a719ab6fd
GarmusD <garmusd@gmail.com>
2022-12-11 08:18:02 +0100

    Serilog sink for NsuConsole

---
+++ NSUWatcher/Logger/Serilog/NsuConsoleMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NSUWatcher.Logger.Serilog
+{
+    public class NsuConsoleEventArgs : EventArgs
+    {
+        public string Output { get; }
+
+        public NsuConsoleEventArgs(string output)
+        {
+            Output = output;
+        }
+    }
+
+    public static class NsuConsoleMessage
+    {
+        public static event EventHandler<NsuConsoleEventArgs> OutputReceived;
+
+        public static void SetOutputLine(string line)
+        {
+            OutputReceived?.Invoke(null, new NsuConsoleEventArgs(line));
+        }
+    }
+}
