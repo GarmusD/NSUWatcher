092087530094f9b4b54b864eaf78042744551640
GarmusD <garmusd@gmail.com>
2022-12-11 08:14:41 +0100

    Support for few more messages from mcu

---
+++ NSUWatcher/Interfaces/MCUCommands/From/IRelayChannelClosed.cs
@@ -0,0 +1,8 @@
+﻿namespace NSUWatcher.Interfaces.MCUCommands.From
+{
+    public interface IRelayChannelClosed : IMessageFromMcu
+    {
+        byte Channel { get; set; }
+        bool IsLocked { get; set; }
+    }
+}
---
+++ NSUWatcher/Interfaces/MCUCommands/From/IRelayChannelOpened.cs
@@ -0,0 +1,8 @@
+﻿namespace NSUWatcher.Interfaces.MCUCommands.From
+{
+    public interface IRelayChannelOpened : IMessageFromMcu
+    {
+        byte Channel { get; set; }
+        bool IsLocked { get; set; }
+    }
+}
