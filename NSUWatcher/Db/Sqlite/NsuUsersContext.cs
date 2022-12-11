194aa0d2615e550847e2b4daf5e5b062f3327c71
GarmusD <garmusd@gmail.com>
2022-12-12 00:42:51 +0100

    Adding EF6 and Sqlite for NSUUsers

---
+++ NSUWatcher/Db/Sqlite/NsuUsersContext.cs
@@ -0,0 +1,15 @@
+﻿using NSUWatcher.Db.Models;
+using System.Data.Entity;
+
+namespace NSUWatcher.Db.Sqlite
+{
+    public class NsuUsersContext : DbContext
+    {
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            
+        }
+        public DbSet<NsuUser> Users { get; set; }
+    }
+}
---
+++ NSUWatcher/Db/Sqlite/SqliteConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace NSUWatcher.Db.Sqlite
+{
+    /*public class SqliteConfiguration : DbConfiguration
+    {
+        public SqliteConfiguration()
+        {
+            
+        }
+    }*/
+}
