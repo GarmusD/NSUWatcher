d2ef3f19a13056e637c7c4f0e339929b9af05ab6
GarmusD <garmusd@gmail.com>
2022-12-11 08:15:45 +0100

    Transport To/From MCU

---
+++ NSUWatcher/Interfaces/IMcuMessageTransport.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace NSUWatcher.Interfaces
+{
+    public interface IMcuMessageTransport : IDisposable
+    {
+        event EventHandler<TransportDataReceivedEventArgs> DataReceived;
+        event EventHandler<TransportStateChangedEventArgs> StateChanged;
+
+        bool IsConnected { get; }
+        bool Start();
+        void Stop();
+        void Send(string command);
+    }
+
+    public class TransportDataReceivedEventArgs : EventArgs
+    {
+        public string Message { get; private set; }
+
+        public TransportDataReceivedEventArgs(string message)
+        {
+            Message = message;
+        }
+    }
+
+    public enum TransportState
+    {
+        Disconnected,
+        NotConnected,
+        Connected,
+        McuHalted
+    }
+
+    public class TransportStateChangedEventArgs : EventArgs
+    {
+        public TransportState State { get; private set; }
+
+        public TransportStateChangedEventArgs(TransportState state)
+        {
+            State = state;
+        }
+    }
+}
