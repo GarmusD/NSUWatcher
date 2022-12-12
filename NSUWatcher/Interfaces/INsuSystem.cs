d1aad30f2cb958e699ca41fd9b3fe9f61b376ac0
GarmusD <garmusd@gmail.com>
2022-12-12 09:46:39 +0100

    Added GetData() method

--- NSUWatcher/Interfaces/INsuSystem.cs
+++ NSUWatcher/Interfaces/INsuSystem.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NSU.Shared.DataContracts;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 
 namespace NSUWatcher.Interfaces
@@ -6,6 +8,7 @@ namespace NSUWatcher.Interfaces
     public interface INsuSystem
     {
         event EventHandler<PropertyChangedEventArgs> StatusChanged;
+        IEnumerator<T> GetData<T>() where T : INSUSysPartDataContract;
     }
 /*    
     public enum ChangedProperty
