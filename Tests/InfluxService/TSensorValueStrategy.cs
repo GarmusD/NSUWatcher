55679549b59b0f7602a509a2eaa5edf8fa2979af
GarmusD <garmusd@gmail.com>
2022-12-12 23:29:08 +0100

    Initial commit

---
+++ Tests/InfluxService/TSensorValueStrategy.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSUWatcher.Services.InfluxDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.InfluxService
+{
+    [TestClass]
+    public class TSensorValueStrategy
+    {
+        [TestMethod]
+        public void LastValueStrategy()
+        {
+            TemperatureSensorEntity tsEntity = new TemperatureSensorEntity(10.0, ValueStrategyType.LastValue);
+            Assert.AreEqual(10.0, tsEntity.Temperature);
+
+            tsEntity.Temperature = 20.5;
+            Assert.AreEqual(20.5, tsEntity.Temperature);
+        }
+
+        [TestMethod]
+        public void AverageValueStrategy()
+        {
+            TemperatureSensorEntity tsEntity = new TemperatureSensorEntity(10.0, ValueStrategyType.AverageValue);
+            tsEntity.Temperature = 20.0;
+            tsEntity.Temperature = 30.0;
+            Assert.AreEqual(20.0, tsEntity.Temperature);
+
+            tsEntity.Temperature = 10.0;
+            tsEntity.Temperature = 20.0;
+            tsEntity.Temperature = 30.0;
+            Assert.AreEqual(20.0, tsEntity.Temperature);
+        }
+
+        [TestMethod]
+        public void MaxValueStrategy()
+        {
+            TemperatureSensorEntity tsEntity = new TemperatureSensorEntity(10.0, ValueStrategyType.MaxValue);
+            Assert.AreEqual(10.0, tsEntity.Temperature);
+
+            tsEntity.Temperature = 15.0;
+            Assert.AreEqual(15.0, tsEntity.Temperature);
+
+            tsEntity.Temperature = -30.0;
+            Assert.AreEqual(15.0, tsEntity.Temperature);
+
+            tsEntity.Temperature = 45.0;
+            Assert.AreEqual(45.0, tsEntity.Temperature);
+
+            tsEntity.Temperature = 21.5;
+            Assert.AreEqual(45.0, tsEntity.Temperature);
+        }
+    }
+}
