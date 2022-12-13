84b2811c193779064760d189c71b835403cbab26
GarmusD <garmusd@gmail.com>
2022-12-13 20:27:28 +0100

    Code cleanup

--- Tests/InfluxService/TSensorValueStrategy.cs
+++ Tests/InfluxService/TSensorValueStrategy.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSUWatcher.Services.InfluxDB;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tests.InfluxService
 {
