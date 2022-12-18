86b353db31dfd8aea7f37420f4fba2dfadf7d544
GarmusD <garmusd@gmail.com>
2022-12-18 21:36:39 +0100

    Implemented INsuUser interface

--- NSUWatcher/NSUUserManagement/NSUUsers.cs
+++ NSUWatcher/NSUUserManagement/NSUUsers.cs
@@ -1,8 +1,30 @@
-﻿using NSUWatcher.Interfaces;
+﻿using Microsoft.Extensions.Logging;
+using NSUWatcher.Db;
+using NSUWatcher.Interfaces.NsuUsers;
 
 namespace NSUWatcher.NSUUserManagement
 {
     public class NSUUsers : INsuUsers
     {
+        private readonly ILoggerFactory _loggerFactory;
+
+        public NSUUsers(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory= loggerFactory;
+        }
+
+        public INsuUser GetUser(string username, string password)
+        {
+            return new NsuWatcherDbContext(_loggerFactory)
+                .NsuUsersDbContext
+                .GetUser(username, PasswordHasher.ComputeHash(password));
+        }
+
+        public bool UserExists(string username)
+        {
+            return new NsuWatcherDbContext(_loggerFactory)
+                .NsuUsersDbContext
+                .UserExists(username);
+        }
     }
 }
