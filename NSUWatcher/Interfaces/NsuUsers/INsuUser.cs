8663b7f0d971112c6f870564cca1c59c1d89b554
GarmusD <garmusd@gmail.com>
2022-12-18 21:22:17 +0100

    Interfaces for NsuUser

---
+++ NSUWatcher/Interfaces/NsuUsers/INsuUser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace NSUWatcher.Interfaces.NsuUsers
+{
+    public enum NsuUserType
+    {
+        User = 0,
+        Admin = 1
+    }
+
+    public interface INsuUser
+    {
+        int Id { get; }
+        string UserName { get; }
+        string Password { get; }
+        NsuUserType UserType { get; }
+        ICollection<string> Permissions { get; }
+    }
+}
---
+++ NSUWatcher/Interfaces/NsuUsers/INsuUsers.cs
@@ -0,0 +1,9 @@
+﻿namespace NSUWatcher.Interfaces.NsuUsers
+{
+#nullable enable
+    public interface INsuUsers
+    {
+        INsuUser? GetUser(string username, string password);
+        bool UserExists(string username);
+    }
+}
