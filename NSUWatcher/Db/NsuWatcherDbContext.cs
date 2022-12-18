b04509e4517974deb76b39b31685661bd932d166
GarmusD <garmusd@gmail.com>
2022-12-18 21:36:08 +0100

    Initial commit

---
+++ NSUWatcher/Db/NsuWatcherDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using NSUWatcher.Db.Sqlite;
+using NSUWatcher.Interfaces.DB;
+using System;
+
+namespace NSUWatcher.Db
+{
+    public class NsuWatcherDbContext : INsuDbContext
+    {
+        private readonly ILoggerFactory _loggerFactory;
+        public NsuWatcherDbContext(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+        
+        public INsuWatcherUsersDbContext NsuUsersDbContext => new NsuWatcherUsersDbContext(_loggerFactory);
+    }
+}
