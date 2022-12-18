18604f4cddca105618b5258262b496c4fba3598f
GarmusD <garmusd@gmail.com>
2022-12-18 21:21:27 +0100

    CommandID support

--- NSUWatcher/Interfaces/MCUCommands/To/IToMcuSystemCommands.cs
+++ NSUWatcher/Interfaces/MCUCommands/To/IToMcuSystemCommands.cs
@@ -1,11 +1,13 @@
 ﻿namespace NSUWatcher.Interfaces.MCUCommands.To
 {
+#nullable enable
     public interface IToMcuSystemCommands
     {
         ICommandToMCU EmptyCommand();
         ICommandToMCU GetMcuStatus();
         ICommandToMCU RequestSnapshot();
         ICommandToMCU PauseBoot();
-        ICommandToMCU SetTime(int year, int month, int day, int hour, int minute, int second);
+        ICommandToMCU SetTime(int year, int month, int day, int hour, int minute, int second, string? commandId);
     }
+#nullable disable
 }
