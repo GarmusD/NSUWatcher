e449e77f04de0c3002d6739a814c46e95c5d5472
GarmusD <garmusd@gmail.com>
2022-12-18 21:39:13 +0100

    Data folder locations

---
+++ NSUWatcher/NSUWatcherFolders.cs
@@ -0,0 +1,10 @@
+﻿namespace NSUWatcher
+{
+    public class NSUWatcherFolders
+    {
+        public const string ConfigFolder = "/etc/nsuwatcher";
+        public const string DataFolder = "/var/lib/nsuwatcher";
+        public const string LogFolder = "/var/log/nsuwatcher";
+        public const string TempFolder = "/tmp";
+    }
+}
