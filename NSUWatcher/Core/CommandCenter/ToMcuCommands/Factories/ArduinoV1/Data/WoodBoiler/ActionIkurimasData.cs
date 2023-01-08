f1ab972ba8eb340115ca413e2a792cfd10bad0c1
GarmusD <garmusd@gmail.com>
2023-01-08 22:45:02 +0100

    New commands

---
+++ NSUWatcher/Core/CommandCenter/ToMcuCommands/Factories/ArduinoV1/Data/WoodBoiler/ActionIkurimasData.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using NSU.Shared;
+using NSUWatcher.Interfaces.MCUCommands;
+
+namespace NSUWatcher.Core.CommandCenter.ToMcuCommands.Factories.ArduinoV1.Data.WoodBoiler
+{
+#nullable enable
+    public readonly struct ActionIkurimasData : ICommandToMcuData
+    {
+        [JsonProperty(JKeys.Generic.Target)]
+        public string Target { get; }
+        [JsonProperty(JKeys.Generic.Action)]
+        public string Action { get; }
+        [JsonProperty(JKeys.Generic.Name)]
+        public string Name { get; }
+        [JsonProperty(JKeys.Generic.Value)]
+        public string? CommandId { get; }
+
+        public ActionIkurimasData(string wbName, string? commandId = null)
+        {
+            Target = JKeys.WoodBoiler.TargetName;
+            Action = JKeys.WoodBoiler.ActionIkurimas;
+            Name = wbName;
+            CommandId = commandId;
+        }
+    }
+}
---
+++ NSUWatcher/Core/CommandCenter/ToMcuCommands/Factories/ArduinoV1/Data/WoodBoiler/SwitchManualModeData.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using NSU.Shared;
+using NSUWatcher.Interfaces.MCUCommands;
+
+namespace NSUWatcher.Core.CommandCenter.ToMcuCommands.Factories.ArduinoV1.Data.WoodBoiler
+{
+#nullable enable
+    public readonly struct SwitchManualModeData : ICommandToMcuData
+    {
+        [JsonProperty(JKeys.Generic.Target)]
+        public string Target { get; }
+        [JsonProperty(JKeys.Generic.Action)]
+        public string Action { get; }
+        [JsonProperty(JKeys.Generic.Name)]
+        public string Name { get; }
+        [JsonProperty(JKeys.Generic.Value)]
+        public string Value { get; }
+        [JsonProperty(JKeys.Generic.CommandID)]
+        public string? CommandId { get; }
+
+        public SwitchManualModeData(string wbName, string switchTarget, string? commandId = null)
+        {
+            Target = JKeys.WoodBoiler.TargetName;
+            Action = JKeys.WoodBoiler.ActionSwitch;
+            Name = wbName;
+            Value = switchTarget;
+            CommandId = commandId;
+        }
+    }
+}
