f4a96fcc2e46224c9f2b0c15c9c50cc23071dfef
GarmusD <garmusd@gmail.com>
2023-01-08 22:55:49 +0100

    Initial wood boiler commands

---
+++ NSUWatcher/Interfaces/MCUCommands/To/IToMcuWoodBoilerCommands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSUWatcher.Interfaces.MCUCommands.To
+{
+    public interface IToMcuWoodBoilerCommands
+    {
+        ICommandToMCU ActionIkurimas(string name);
+        ICommandToMCU LadomatSwitchManualMode(string name);
+        ICommandToMCU ExhaustFanSwitchManualMode(string name);
+    }
+}
