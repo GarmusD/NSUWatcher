1403235fc7eb5baffd052d07310bd4f0f866e935
GarmusD <garmusd@gmail.com>
2023-01-08 22:41:49 +0100

    Switch expression

--- NSUWatcher/Core/CommandCenter/MessagesFromMcu/Factory/ArduinoV1/ComfortZoneMessages.cs
+++ NSUWatcher/Core/CommandCenter/MessagesFromMcu/Factory/ArduinoV1/ComfortZoneMessages.cs
@@ -5,33 +5,36 @@ using NSU.Shared;
 
 namespace NSUWatcher.CommandCenter.MessagesFromMcu.Factory.ArduinoV1
 {
+#nullable enable
     public class ComfortZoneMessages : IFromArduinoV1Base
     {
-        public IMessageFromMcu TryFindMessage(JObject command)
+        public IMessageFromMcu? TryFindMessage(JObject command)
         {
-            string source = (string)command[JKeys.Generic.Source];
+            string source = (string)command[JKeys.Generic.Source]!;
             if (source != JKeys.ComfortZone.TargetName) return null;
 
-            string action = (string)command[JKeys.Generic.Action];
+            string action = (string)command[JKeys.Generic.Action]!;
 
-            switch (action)
+            return action switch
             {
-                case JKeys.Action.Snapshot: return command.ToObject<ComfortZoneSnapshot>();
-                case JKeys.Action.Info: return GetInfoByContent(command);
-                default: return null;
+                JKeys.Action.Snapshot => command.ToObject<ComfortZoneSnapshot>(),
+                JKeys.Action.Info => GetInfoByContent(command),
+                _ => null,
             };
+            ;
         }
 
-        private static IMessageFromMcu GetInfoByContent(JObject command)
+        private static IMessageFromMcu? GetInfoByContent(JObject command)
         {
-            string content = (string)command[JKeys.Generic.Content];
-            switch (content)
+            string? content = (string?)command[JKeys.Generic.Content];
+            if(content == null) { return null; }
+            return content switch
             {
-                case JKeys.ComfortZone.CurrentRoomTemp: return command.ToObject<ComfortZoneRoomTempInfo>();
-                case JKeys.ComfortZone.CurrentFloorTemp: return command.ToObject<ComfortZoneFloorTempInfo>();
-                case JKeys.ComfortZone.ActuatorOpened: return command.ToObject<ComfortZoneActuatorStatus>();
-                case JKeys.ComfortZone.LowTempMode: return command.ToObject<ComfortZoneLowTempMode>();
-                default: return null;
+                JKeys.ComfortZone.CurrentRoomTemp => command.ToObject<ComfortZoneRoomTempInfo>(),
+                JKeys.ComfortZone.CurrentFloorTemp => command.ToObject<ComfortZoneFloorTempInfo>(),
+                JKeys.ComfortZone.ActuatorOpened => command.ToObject<ComfortZoneActuatorStatus>(),
+                JKeys.ComfortZone.LowTempMode => command.ToObject<ComfortZoneLowTempMode>(),
+                _ => null,
             };
         }
     }
